4ae171b60c3e2314e452a3a0d6a7a7482ed1c246
Sandeep Malik <sandeep.malik@gmail.com>
2019-01-28 18:01:47 -0800

    adding documentation

--- build/docker/app_run.sh
+++ build/docker/app_run.sh
@@ -6,12 +6,20 @@ HZ_PORT=5701
 NUM_INSTANCES=${NUM_INSTANCES:-1}
 APP_ROOT=/dist
 BUILD_DIR=${PWD}/..
-LOGS_DIR=${LOGS_DIR:-${BUILD_DIR}/../bigben_logs}
-echo "app logs dir set to ${LOGS_DIR}"
+LOGS_DIR=${LOGS_DIR:-${BUILD_DIR}/../../bigben_logs}
+
+HOST_IP=${HOST_IP:-`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`}
 CASSANDRA_SEED_IPS=${CASSANDRA_SEED_IPS:-${HOST_IP}}
 HZ_MEMBER_IPS=${HZ_MEMBER_IPS:-${HOST_IP}}
 
-HOST_IP=${HOST_IP:-`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`}
+DEFAULT_JAVA_OPTS="-server -XX:+UnlockExperimentalVMOptions -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication"
+JAVA_OPTS=${JAVA_OPTS}
+
+if [[ "x${JAVA_OPTS}" != "x" ]]; then
+    JAVA_OPTS="${DEFAULT_JAVA_OPTS} ${JAVA_OPTS}"
+else
+    JAVA_OPTS="${DEFAULT_JAVA_OPTS}"
+fi
 
 echo HOST_IP: ${HOST_IP}, SERVER_PORT: ${SERVER_PORT}, \
 HZ_MEMBER_IPS: ${HZ_MEMBER_IPS}, CASSANDRA_SEED_IPS: ${CASSANDRA_SEED_IPS}, \
@@ -35,21 +43,21 @@ function start() {
         app_port=$((${SERVER_PORT} + 101 * $((i - 1))))
         hz_port=$((${HZ_PORT} + i - 1))
         echo "starting ${APP_CONTAINER_NAME}_$app_port at app port: $app_port, hz port: $hz_port"
-        docker run --rm \
+        docker run -d --rm \
         -p ${app_port}:${SERVER_PORT} \
         -p ${hz_port}:${HZ_PORT} \
         -v ${BUILD_DIR}/bin/bigben.yaml:${APP_ROOT}/bigben-config.yaml \
         -v ${BUILD_DIR}/configs/overrides.yaml:${APP_ROOT}/bigben-overrides.yaml \
         -v ${BUILD_DIR}/configs/log4j.xml:${APP_ROOT}/log4j.xml \
-        -v ${LOGS_DIR}:${LOGS_DIR} \
-        -e ${LOGS_DIR}:${LOGS_DIR} \
+        -v ${LOGS_DIR}:${APP_ROOT}/logs \
+        -e LOG_FILE="${APP_ROOT}/logs/bigben_app_${app_port}.log" \
         -e HOST_IP="${HOST_IP}" \
         -e CASSANDRA_SEED_IPS="${CASSANDRA_SEED_IPS}" \
         -e HZ_MEMBER_IPS="${HZ_MEMBER_IPS}" \
         -e CONFIGS='bigben-overrides,bigben-config' \
         -e SERVER_PORT=${SERVER_PORT} \
         -e APP_PORT=${app_port} \
-        -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST_IP}:${hz_port}" \
+        -e JAVA_OPTS="${JAVA_OPTS} -Dhazelcast.local.publicAddress=${HOST_IP}:${hz_port}" \
         --name "${APP_CONTAINER_NAME}_$app_port" sandeepmalik/bigben:1
         let i=i+1
     done
